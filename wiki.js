4ca5157dc219761989c4356b3aa779fc1f8b117c
ch1bi <maine1191@gmail.com>
2017-01-23 00:57:32 -0500

    added html string to wiki.js

--- wiki.js
+++ wiki.js
@@ -21,13 +21,28 @@ xRequest.onreadystatechange = function(){
 
 if(xRequest.readyState == 4 && xRequest.status == 200){
             
-        var data = xRequest.responseText;
-        //console.log(data);
+            var data = xRequest.responseText;
+            var json = JSON.parse(data);
+            //console.log(json);
+            var keys = Object.keys(json.query.pages);
+            //console.log(keys);
     
-        var json = JSON.parse(data);
-        console.log(json);
-      
-}
+            var resultString = "";
+            
+            for(var i = 0; i < keys.length; i++){
+                  
+                resultString += "<a href='https://en.wikipedia.org/wiki"+json.query.pages[keys[i]].title+"'>";
+                resultString += "<div class='outer'>";
+                resultString += "<p class='title'>";
+                resultString += json.query.pages[keys[i]].title;
+                
+                resultString += "</div>";
+                resultString += "</a>";
+            }
+    
+    data.innerHTML = resultString;
+    
+    }
 }
 xRequest.open("GET", url);
 xRequest.send(null);
